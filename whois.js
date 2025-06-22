a5759bfc8bc84d68326a0a0d50f48e64508d3621
kukuji <3377460264@qq.com>
2025-06-22 14:44:04 +0800

    Update whois.js

--- whois.js
+++ whois.js
@@ -11,7 +11,7 @@ export class WhoisQuery extends plugin {
       priority: 10,
       rule: [
         {
-          reg: /^#?whois查询\s?.+$/i,
+          reg: /^#?(whois|域名信息)查询\s?.+$/i,
           fnc: 'queryWhois'
         }
       ]
@@ -19,7 +19,7 @@ export class WhoisQuery extends plugin {
   }
 
   async queryWhois() {
-    let originalDomain = this.e.msg.replace(/^#?whois查询\s?/i, '').trim()
+    let originalDomain = this.e.msg.replace(/^#?(whois|域名信息)查询\s?/i, '').trim()
     if (!originalDomain) {
       await this.reply('请输入要查询的域名')
       return
